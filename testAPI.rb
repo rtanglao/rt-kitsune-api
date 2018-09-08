f1748bdc8c4b0e028c65f2f0f7b97834f3fe4153
Roland Tanglao <roland@rolandtanglao.com>
2018-09-07 21:23:50 -0700

    it does something

---
+++ testAPI.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'rubygems'
+require 'typhoeus'
+require 'awesome_print'
+
+def getKitsuneResponse(url, params)
+  # url = "https://api.instagram.com/v1/" + url
+  result = Typhoeus::Request.get(url,
+    :params => params )
+  try_count = 0
+  begin
+    x = JSON.parse(result.body)
+  rescue JSON::ParserError => e
+    try_count += 1
+    if try_count < 4
+      $stderr.printf("JSON::ParserError exception, retry:%d\n",\
+                     try_count)
+      sleep(10)
+      retry
+    else
+      $stderr.printf("JSON::ParserError exception, retrying FAILED\n")
+      # raise e
+      x = nil
+    end
+  end
+  return x
+end
+
+ url_params = {
+      :format => "json",
+      :product => "firefox", # source: http://code.flickr.net/2008/09/04/whos-on-first/
+      :created_date => "09/05/2018",
+   #   :distance => "5000",
+   #   :min_timestamp => min_taken_date_str,
+   #   :max_timestamp => max_taken_date_str,
+   #   :count => 100
+} 
+
+url = "https://support.mozilla.org/api/2/question/"
+questions  = getKitsuneResponse(url, url_params)
+ap questions
