2e4e58574de210d78e5f20457a46afaede87b8f7
Roland Tanglao <roland@rolandtanglao.com>
2018-09-07 22:00:03 -0700

    Update testAPI.rb

--- testAPI.rb
+++ testAPI.rb
@@ -31,8 +31,8 @@ end
       :product => "firefox", # source: http://code.flickr.net/2008/09/04/whos-on-first/
       :created_date => "09/05/2018",
       :locale => "en-US",
-      #:ordering => "-updated",
-      :ordering => "+created",
+      :ordering => "+updated",
+     # :ordering => "+created",
      # :max_questions => 10,
    #   :created => 2,
     #  :sortby => 2
