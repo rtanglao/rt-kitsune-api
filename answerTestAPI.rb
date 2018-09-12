7c5f32a1018ef693a91747fdbce7e2025c8e8add
Roland Tanglao <roland@rolandtanglao.com>
2018-09-11 18:50:03 -0700

    thanks jscher2000 for https://github.com/jscher2000/My-SuMo-Questions/blob/master/myq.html which allowed me to figure out the answers API

---
+++ answerTestAPI.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'rubygems'
+require 'typhoeus'
+require 'awesome_print'
+
+def getKitsuneResponse(url, params)
+  result = Typhoeus::Request.get(url,
+    :params => params )
+  try_count = 0
+  begin
+    x = JSON.parse(result.body)
+  rescue JSON::ParserError => e
+    try_count += 1
+    if try_count < 4
+      $stderr.printf("JSON::ParserError exception, retry:%d\n",\
+                     try_count)
+      sleep(10)
+      retry
+    else
+      $stderr.printf("JSON::ParserError exception, retrying FAILED\n")
+      # raise e
+      x = nil
+    end
+  end
+  return x
+end
+
+ url_params = {
+      :format => "json",
+     # :product => "firefox", # source: http://code.flickr.net/2008/09/04/whos-on-first/
+      #:created_date => "09/05/2018",
+      #:created_date => "2018-09-05",
+      :question =>  1232908,
+   #   :ordering => "created",
+    #  :ordering => "+created",
+     # :max_questions => 10,
+   #   :created => 2,
+ #     :sortby => 1
+} 
+
+url = "https://support.mozilla.org/api/2/answer/"
+answer  = getKitsuneResponse(url, url_params)
+ap answer
+
