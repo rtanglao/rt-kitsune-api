ccabd4aeca47a282c6e9bfc77ca3b3a3d0676ee2
Roland Tanglao <roland@rolandtanglao.com>
2018-09-11 17:21:52 -0700

    fix typo

--- open-day-in-browser.rb
+++ open-day-in-browser.rb
@@ -60,7 +60,7 @@ questionsColl.find(:created =>
   Launchy.open("http://support.mozilla.org/questions/" + id.to_s)
   sleep(0.5)
 end
-  logger.debug "number of tabs fo this day:" + number_of_tabs_for_this_day.to_s
+  logger.debug "number of tabs for this day:" + number_of_tabs_for_this_day.to_s
 
   
 
