5739072cbf2a02e20f4779757047764b1d700440
Roland Tanglao <roland@rolandtanglao.com>
2018-09-16 20:05:30 -0700

    add a script to print tags

---
+++ print-csv-tags-unixtime.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'rubygems'
+require 'awesome_print'
+require 'time'
+require 'date'
+require 'mongo'
+require 'logger'
+require 'launchy'
+
+logger = Logger.new(STDERR)
+logger.level = Logger::DEBUG
+Mongo::Logger.logger.level = Logger::FATAL
+MONGO_HOST = ENV["MONGO_HOST"]
+xfraise(StandardError,"Set Mongo hostname in ENV: 'MONGO_HOST'") if !MONGO_HOST
+MONGO_PORT = ENV["MONGO_PORT"]
+raise(StandardError,"Set Mongo port in ENV: 'MONGO_PORT'") if !MONGO_PORT
+MONGO_USER = ENV["MONGO_USER"]
+# raise(StandardError,"Set Mongo user in ENV: 'MONGO_USER'") if !MONGO_USER
+MONGO_PASSWORD = ENV["MONGO_PASSWORD"]
+# raise(StandardError,"Set Mongo user in ENV: 'MONGO_PASSWORD'") if !MONGO_PASSWORD
+SUMO_QUESTIONS_DB = ENV["SUMO_QUESTIONS_DB"]
+raise(StandardError,\
+      "Set SUMO questions  database name in ENV: 'SUMO_QUESTIONS_DB'") \
+if !SUMO_QUESTIONS_DB
+
+db = Mongo::Client.new([MONGO_HOST], :database => SUMO_QUESTIONS_DB)
+if MONGO_USER
+  auth = db.authenticate(MONGO_USER, MONGO_PASSWORD)
+  if !auth
+    raise(StandardError, "Couldn't authenticate, exiting")
+    exit
+  end
+end
+
+if ARGV.length < 6
+  puts "usage: #{$0} yyyy mm dd yyyy mm dd" # day you want to open
+  exit
+end
+
+questionsColl = db[:questions]
+MIN_DATE = Time.local(ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i, 0, 0) # may want Time.utc if you don't want local time
+MAX_DATE = Time.local(ARGV[3].to_i, ARGV[4].to_i, ARGV[5].to_i, 23, 59) # may want Time.utc if you don't want local time
+
+print "id,tag,unixtime\n"
+questionsColl.find(:created =>
+  {
+    :$gte => MIN_DATE,
+    :$lte => MAX_DATE },
+  ).sort(
+  {"id"=> 1}
+  ).projection(
+  {
+    "id" => 1,
+    "tags" => 1,
+    "created" => 1
+  }).each do |q|
+  id = q["id"]
+
+  logger.debug "QUESTION id:" + id.to_s
+  tags = q["tags"]
+
+  tags.each do |t|
+    print id.to_s + "," + t["slug"] + "," + q["created"].to_i.to_s + "\n"
+  end
+
+end
+
+  
+
