cc15673ac112111dd84fbd58192c52235b6ca02a
Roland Tanglao <roland@rolandtanglao.com>
2018-10-20 22:47:10 -0700

    Create firefox62-most-common-words-bar-graph.r

---
+++ VISUALIZATIONS/firefox62-most-common-words-bar-graph.r
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(stringr)
+library(tokenizers)
+library(tidytext)
+library(textclean)
+ff62_first3weeks = 
+  read_csv(
+    "https://raw.githubusercontent.com/rtanglao/rt-kitsune-api/master/VISUALIZATIONS/created-20october2018-5-25september-ff-desktop-questions-id-content-created-product-tags-title-topic.csv")
+ff62_first3weeks_title_content_concatenated <-
+  ff62_first3weeks %>% 
+  unite(text, title, content, sep = " ")
+ff62_first3weeks_title_content_concatenated <-
+  ff62_first3weeks_title_content_concatenated %>% 
+  mutate(text = replace_html(text))
+tidy_ff62_first3weeks <-
+  ff62_first3weeks_title_content_concatenated %>% 
+  unnest_tokens(word, text)
+data(stop_words)
+tidy_ff62_first3weeks <-
+  tidy_ff62_first3weeks %>% 
+  anti_join(stop_words)
+ff62_count <- 
+  tidy_ff62_first3weeks %>% 
+    count(word, sort = TRUE)
+ff62_count %>% 
+  filter(n > 100) %>% 
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() 
+# which produces Rplot13
