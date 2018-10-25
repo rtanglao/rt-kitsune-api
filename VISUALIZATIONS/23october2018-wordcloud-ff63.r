f75576f5d8773d277e95bc43e088292362779990
Roland Tanglao <roland@rolandtanglao.com>
2018-10-24 22:18:20 -0700

    fix formatting

--- VISUALIZATIONS/23october2018-wordcloud-ff63.r
+++ VISUALIZATIONS/23october2018-wordcloud-ff63.r
@@ -1,17 +1,19 @@
 library(mongolite)
+library(tidyverse)
 m <- mongo("questions",
            url = "mongodb://127.0.0.1:27017/ff62questions")
-MIN_DATE = strptime("2018-10-23 00:00:00 -0700", format = '%Y-%m-%d %H:%M:%S %z', 
-                    tz = "UTC" )
+MIN_DATE = strptime("2018-10-23 00:00:00 -0700", format = '%Y-%m-%d %H:%M:%S %z',
+                    tz = "UTC")
 MIN_DATE_STR = strftime(MIN_DATE, "%Y-%m-%dT%H:%M:%SZ", 'UTC')
 MAX_DATE = strptime("2018-10-23 23:59:59 -0700",  format = '%Y-%m-%d %H:%M:%S %z',
                     tz = "UTC")
 MAX_DATE_STR = strftime(MAX_DATE, "%Y-%m-%dT%H:%M:%SZ", 'UTC')
-#MAX_DATE_STR = "2018-10-23T23:59:59-0700"
-query_str = 
+query_str =
   sprintf(
-    '{ "created" : { "$gt" : { "$date" : "%s"}, "$lte" : {"$date" : "%s"}}}', 
-          MIN_DATE_STR, MAX_DATE_STR)
+    '{ "created" : { "$gt" : { "$date" : "%s"}, "$lte" : {"$date" : "%s"}}}',
+    MIN_DATE_STR,
+    MAX_DATE_STR
+  )
 
 one_day_of_data <-
   m$find(
