4c7569ef5852d713b2cae7be6d7124dcda7eb324
Roland Tanglao <roland@rolandtanglao.com>
2019-01-09 17:49:51 -0800

    add antivirus-only.r

---
+++ VISUALIZATIONS/antivirus-only.r
@@ -0,0 +1,13 @@
+antivirus_only <- 
+  function(previous_df_release_release_week_day_number)
+    {
+      return(
+      previous_df_release_release_week_day_number %>% 
+        filter(
+        grepl("antivirus|anti-virus|anti\\svirus", text, ignore.case = TRUE) |
+        grepl("kaspersky|sophos|avast|avg", text, ignore.case = TRUE) |
+        grepl("bitdefender|norton|mcafee|secureanywhere", text, ignore.case = TRUE) |
+        grepl("trendmicro|trend\\smicro", text, ignore.case = TRUE)
+        )
+      )
+  }
