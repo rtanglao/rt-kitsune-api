ea53c24cb0cd345cd2ff288145694bf51d8f48bb
Roland Tanglao <roland@rolandtanglao.com>
2019-01-10 20:38:27 -0800

    add nod32, avira and other missing vendors

--- VISUALIZATIONS/antivirus-only.r
+++ VISUALIZATIONS/antivirus-only.r
@@ -7,7 +7,9 @@ antivirus_only <-
         grepl("antivirus|anti-virus|anti\\svirus", text, ignore.case = TRUE) |
         grepl("kaspersky|sophos|avast|avg", text, ignore.case = TRUE) |
         grepl("bitdefender|norton|mcafee|secureanywhere", text, ignore.case = TRUE) |
-        grepl("trendmicro|trend\\smicro", text, ignore.case = TRUE)
+        grepl("trendmicro|trend\\smicro", text, ignore.case = TRUE) |
+        grepl("nod32|avira|secureanywhere|fsecure|f-secure|symantec", 
+              text, ignore.case = TRUE)
         )
       )
   }
