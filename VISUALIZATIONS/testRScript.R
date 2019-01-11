c36b0ffb14b326a30eeb21a40c1a0d5e7a280ea7
Roland Tanglao <roland@rolandtanglao.com>
2019-01-10 19:21:01 -0800

    change source to debugSource()

--- VISUALIZATIONS/testRScript.R
+++ VISUALIZATIONS/testRScript.R
@@ -1,3 +1,3 @@
 commandArgs <- function(...) return(c("FF61", "2018", "6", "26", "FF62", "2018", "9", "5", "no"))
-source("plot-antivirus-mentions-by-3weeks.r")
+debugSource("plot-antivirus-mentions-by-3weeks.r")
 rm(commandArgs)
